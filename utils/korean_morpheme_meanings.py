b576637849345185314ab9254bed67e4c1a2b1aa
Becky Mathew <codebecky@gmail.com>
2020-07-13 13:25:38 -0700

    Not finished -- trying to label Korean morphemes

---
+++ utils/korean_morpheme_meanings.py
@@ -0,0 +1,23 @@
+{
+    "ep_았": "",
+    "jcr_고": "",
+    "ecs_면": "",
+    "px_지": "", # M2TA_064-s16, M2TA_064-s22, M2TA_064-s58 TODO: no idea...
+    "ecx_게": "CAUSATIVE", # https://en.wiktionary.org/wiki/%EA%B2%8C etymology 6 -- causative / passive
+    "paa_있": "", # TODO: can't find an example
+    "etn_기": "DERIVATION", # nominalizer
+    "ecx_지": "CONNECTOR", # used with ji + anhda / motha negation
+    "nbn_수": "ABILITY", # exclusively used with verb + su + iss/eops-da pattern
+    "ecx_고": "CONNECTOR", 
+    "px_있"	"AUXILIARY", # sometimes used for progressive, ability/inability, etc
+    "ef_ㄴ다": "DECLARATIVE", # plain present indicative for action verbs
+    "ecs_어": "CONNECTOR", # https://www.howtostudykorean.com/unit-3-intermediate-korean-grammar/lessons-67-75/lesson-70/ TODO
+    "ecx_어": "TENSE", # It's not exactly tense, but verbs must conjugate with -a or -o at the end in -a/-o/ya register TODO
+    "etm_ㄹ": "DERIVATION", # adnominalizer (used with future tense? TODO)
+    "jp_이": "VERB_DERIVATION", # predicative maker, appears earlier than other derivation slots
+    "ep_ㅆ": "TENSE", # pre-final past tense marker
+    "ef_다": "FINAL", # TODO
+    "etm_ㄴ": "DERIVATION" # adnominalizer
+}
+
+def morpheme_meaning(grapheme, label):
