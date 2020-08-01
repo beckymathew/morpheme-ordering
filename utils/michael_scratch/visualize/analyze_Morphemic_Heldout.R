a21e524175131a3ed1dece6980d5174b1d85d40f
Michael Hahn <mhahn29@gmail.com>
2020-08-01 11:05:27 +0800

    code

--- utils/michael_scratch/visualize/analyze_Morphemic_Heldout.R
+++ utils/michael_scratch/visualize/analyze_Morphemic_Heldout.R
@@ -10,49 +10,49 @@ PURPLE = "#C77CFF"
 SCALE = c( GREEN, RED, BLUE, PURPLE)
 ######################################
 
-data = read.csv("results.tsv", sep="\t")
-data = data %>% filter(Script == "forWords_Japanese_RandomOrder_Normalized_FullData_Heldout.py")
-
-unigramCE = mean(data$UnigramCE)
-
-data$Type = ifelse(data$Model %in% c("REAL", "RANDOM", "REVERSE"), as.character(data$Model), "Optimized")
-data$Type = ifelse(data$Type %in% c("REAL"), "Real", as.character(data$Type))
-data$Type = ifelse(data$Type %in% c("RANDOM"), "Random", as.character(data$Type))
-data$Type = ifelse(data$Type %in% c("REVERSE"), "Reverse", as.character(data$Type))
-
-data_ = data %>% group_by(Distance, Type) %>% summarise(MI=median(MI))
-data_ = data_ %>% filter(Distance < 5)
-plot = ggplot(data_, aes(x=1+Distance, y=MI, color=Type)) + geom_line()
-plot = plot + theme_bw()
-plot = plot + xlab("Distance") + ylab("Mutual Information")
-plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
-plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
-plot = plot + theme(axis.line = element_line(colour = "black"),
-            panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.border = element_blank(),
-                    panel.background = element_blank()) + scale_colour_manual(values=SCALE) + scale_fill_manual(values=SCALE)
-ggsave(plot, file=paste("../figures/Japanese-suffixes-byMorphemes-it-heldout.pdf", sep=""), height=4, width=4)
-
-data = read.csv("results_interpolated.tsv", sep="\t")
-
-
-data = data %>% filter(Script == "forWords_Japanese_RandomOrder_Normalized_FullData_Heldout.py")
-
-data = data %>% group_by(Type, Memory) %>% summarise(Surprisal=unigramCE-median(MI))
-
-plot = ggplot(data, aes(x=Memory, y=Surprisal, color=Type)) + geom_line() #size=2)
-plot = plot + theme_bw()
-plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
-plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
-plot = plot + theme(axis.line = element_line(colour = "black"),
-            panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.border = element_blank(),
-                    panel.background = element_blank()) + scale_colour_manual(values=SCALE) + scale_fill_manual(values=SCALE)
-ggsave(plot, file=paste("../figures/Japanese-suffixes-byMorphemes-memsurp-heldout.pdf", sep=""), height=4, width=4)
-
-
+#data = read.csv("results.tsv", sep="\t")
+#data = data %>% filter(Script == "forWords_Japanese_RandomOrder_Normalized_FullData_Heldout.py")
+#
+#unigramCE = mean(data$UnigramCE)
+#
+#data$Type = ifelse(data$Model %in% c("REAL", "RANDOM", "REVERSE"), as.character(data$Model), "Optimized")
+#data$Type = ifelse(data$Type %in% c("REAL"), "Real", as.character(data$Type))
+#data$Type = ifelse(data$Type %in% c("RANDOM"), "Random", as.character(data$Type))
+#data$Type = ifelse(data$Type %in% c("REVERSE"), "Reverse", as.character(data$Type))
+#
+#data_ = data %>% group_by(Distance, Type) %>% summarise(MI=median(MI))
+#data_ = data_ %>% filter(Distance < 5)
+#plot = ggplot(data_, aes(x=1+Distance, y=MI, color=Type)) + geom_line()
+#plot = plot + theme_bw()
+#plot = plot + xlab("Distance") + ylab("Mutual Information")
+#plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
+#plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
+#plot = plot + theme(axis.line = element_line(colour = "black"),
+#            panel.grid.major = element_blank(),
+#                panel.grid.minor = element_blank(),
+#                panel.border = element_blank(),
+#                    panel.background = element_blank()) + scale_colour_manual(values=SCALE) + scale_fill_manual(values=SCALE)
+#ggsave(plot, file=paste("../figures/Japanese-suffixes-byMorphemes-it-heldout.pdf", sep=""), height=4, width=4)
+#
+#data = read.csv("results_interpolated.tsv", sep="\t")
+#
+#
+#data = data %>% filter(Script == "forWords_Japanese_RandomOrder_Normalized_FullData_Heldout.py")
+#
+#data = data %>% group_by(Type, Memory) %>% summarise(Surprisal=unigramCE-median(MI))
+#
+#plot = ggplot(data, aes(x=Memory, y=Surprisal, color=Type)) + geom_line() #size=2)
+#plot = plot + theme_bw()
+#plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
+#plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
+#plot = plot + theme(axis.line = element_line(colour = "black"),
+#            panel.grid.major = element_blank(),
+#                panel.grid.minor = element_blank(),
+#                panel.border = element_blank(),
+#                    panel.background = element_blank()) + scale_colour_manual(values=SCALE) + scale_fill_manual(values=SCALE)
+#ggsave(plot, file=paste("../figures/Japanese-suffixes-byMorphemes-memsurp-heldout.pdf", sep=""), height=4, width=4)
+#
+#
 ######################################
 
 data = read.csv("results_auc.tsv", sep="\t")
