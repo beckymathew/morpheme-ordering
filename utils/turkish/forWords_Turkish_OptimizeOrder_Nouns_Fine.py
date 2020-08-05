ce124246c3e462939733d1f7f81fa6f533cc686e
beckymathew <codebecky@gmail.com>
2020-08-05 10:15:27 -0700

    Scripts for turkish nouns and crossed scripts (coarse representation, fine surprisal)

---
+++ utils/turkish/forWords_Turkish_OptimizeOrder_Nouns_Fine.py
@@ -0,0 +1,183 @@
+# based on yWithMorphologySequentialStreamDropoutDev_Ngrams_Log.py
+
+import random
+import sys
+from estimateTradeoffHeldout import calculateMemorySurprisalTradeoff
+
+objectiveName = "LM"
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", dest="language", type=str, default="Japanese-GSD_2.4")
+parser.add_argument("--model", dest="model", type=str)
+parser.add_argument("--alpha", dest="alpha", type=float, default=1.0)
+parser.add_argument("--gamma", dest="gamma", type=int, default=1)
+parser.add_argument("--delta", dest="delta", type=float, default=1.0)
+parser.add_argument("--cutoff", dest="cutoff", type=int, default=4)
+parser.add_argument("--idForProcess", dest="idForProcess", type=int, default=random.randint(0,10000000))
+import random
+
+
+
+args=parser.parse_args()
+print(args)
+
+
+assert args.alpha >= 0
+assert args.alpha <= 1
+assert args.delta >= 0
+assert args.gamma >= 1
+
+
+
+
+
+myID = args.idForProcess
+
+
+TARGET_DIR = "results/"+__file__.replace(".py", "")
+
+
+
+posUni = set() 
+
+posFine = set() 
+
+def getRepresentation(lemma):
+    return lemma
+
+from math import log, exp
+from random import random, shuffle, randint, Random, choice
+
+header = ["index", "word", "lemma", "posUni", "posFine", "morph", "head", "dep", "_", "_"]
+
+from corpusIterator_V import CorpusIterator_V
+
+originalDistanceWeights = {}
+
+morphKeyValuePairs = set()
+
+vocab_lemmas = {}
+
+import turkish_noun_segmenter
+def processVerb(verb, data_):
+    # assumption that each verb is a single word
+   for vb in verb:
+      labels = vb["morph"]
+      morphs = turkish_noun_segmenter.get_abstract_morphemes(labels)
+      morphs[0] = vb["lemma"] # replace "ROOT" w actual root
+      data_.append(morphs)
+
+corpusTrain = CorpusIterator_V(args.language,"train", storeMorph=True).iterator(rejectShortSentences = False)
+corpusDev = CorpusIterator_V(args.language,"dev", storeMorph=True).iterator(rejectShortSentences = False)
+
+pairs = set()
+counter = 0
+data_train = []
+data_dev = []
+for corpus, data_ in [(corpusTrain, data_train), (corpusDev, data_dev)]:
+  for sentence in corpus:
+    verb = []
+    for line in sentence:
+       if line["posUni"] == "VERB":
+          verb.append(line)
+          processVerb(verb, data_)
+          verb = []
+
+words = []
+
+affixFrequency = {}
+for verbWithAff in data_train:
+  for affix in verbWithAff[1:]:
+    affixLemma = getRepresentation(affix)
+    affixFrequency[affixLemma] = affixFrequency.get(affixLemma, 0)+1
+
+
+itos = set()
+for data_ in [data_train, data_dev]:
+  for verbWithAff in data_:
+    for affix in verbWithAff[1:]:
+      itos.add(getRepresentation(affix))
+itos = sorted(list(itos))
+stoi = dict(list(zip(itos, range(len(itos)))))
+
+itos_ = itos[::]
+shuffle(itos_)
+weights = dict(list(zip(itos_, [2*x for x in range(len(itos_))]))) # abstract slot
+
+
+def calculateTradeoffForWeights(weights):
+    # Order the datasets based on the given weights
+    train = []
+    dev = []
+    for data, processed in [(data_train, train), (data_dev, dev)]:
+      for verb in data:
+         affixes = verb[1:]
+         affixes = sorted(affixes, key=lambda x:weights.get(getRepresentation(x), 0)) 
+         for ch in [verb[0]] + affixes:
+            processed.append(ch) 
+         processed.append("EOS")
+         for _ in range(args.cutoff+2):
+           processed.append("PAD")
+         processed.append("SOS")
+
+    auc, devSurprisalTable = calculateMemorySurprisalTradeoff(train, dev, args)
+    return auc, devSurprisalTable
+   
+
+import os
+for iteration in range(1):
+  # Randomly select a morpheme whose position to update
+  coordinate=choice(itos)
+
+  # Stochastically filter out rare morphemes
+  while affixFrequency.get(coordinate, 0) < 10 and random() < 0.95:
+     coordinate = choice(itos)
+
+  # This will store the minimal AOC found so far and the corresponding position
+  mostCorrect, mostCorrectValue = 1e100, None
+
+  # Iterate over possible new positions
+  for newValue in [-1] + [2*x+1 for x in range(len(itos))] + [weights[coordinate]]:
+
+     # Stochastically exclude positions to save compute time
+     if random() < 0.9 and newValue != weights[coordinate]:
+        continue
+     print(newValue, mostCorrect, coordinate, affixFrequency.get(coordinate,0))
+     # Updated weights, assuming the selected morpheme is moved to the position indicated by `newValue`.
+     weights_ = {x : y if x != coordinate else newValue for x, y in weights.items()}
+
+     # Calculate AOC for this updated assignment
+     resultingAOC, _ = calculateTradeoffForWeights(weights_)
+
+     # Update variables if AOC is smaller than minimum AOC found so far
+     if resultingAOC < mostCorrect:
+        mostCorrectValue = newValue
+        mostCorrect = resultingAOC
+  print(iteration, mostCorrect)
+  weights[coordinate] = mostCorrectValue
+  itos_ = sorted(itos, key=lambda x:weights[x])
+  weights = dict(list(zip(itos_, [2*x for x in range(len(itos_))])))
+  print(weights)
+  for x in itos_:
+     if affixFrequency.get(x,0) < 10:
+       continue
+     print("\t".join([str(y) for y in [x, weights[x], affixFrequency.get(x,0)]]))
+  if (iteration + 1) % 50 == 0:
+     _, surprisals = calculateTradeoffForWeights(weights_)
+
+     if os.path.exists(TARGET_DIR):
+      with open(TARGET_DIR+"/optimized_"+__file__+"_"+str(myID)+".tsv", "w") as outFile:
+          print(iteration, mostCorrect, str(args), surprisals, file=outFile)
+          for key in itos_:
+            print(key, weights[key], file=outFile)
+     else:
+       os.makedirs(TARGET_DIR)
+       with open(TARGET_DIR+"/optimized_"+__file__+"_"+str(myID)+".tsv", "w") as outFile:
+          print(iteration, mostCorrect, str(args), surprisals, file=outFile)
+          for key in itos_:
+            print(key, weights[key], file=outFile)
+  
+
+
+
---
+++ utils/turkish/forWords_Turkish_RandomOrder_Nouns_Coarse_FineSurprisal.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Estimate memory-surprisal tradeoff 
+
+import random
+import sys
+from estimateTradeoffHeldout import calculateMemorySurprisalTradeoff
+from math import log, exp
+from corpusIterator_V import CorpusIterator_V
+from random import shuffle, randint, Random, choice
+
+
+
+objectiveName = "LM"
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", dest="language", type=str, default="Japanese-GSD_2.4")
+
+# May be REAL, RANDOM, REVERSE, or a pointer to a file containing an ordering grammar.
+parser.add_argument("--model", dest="model", type=str)
+
+# parameters for n-gram smoothing. See also estimateTradeoffHeldout.py
+parser.add_argument("--alpha", dest="alpha", type=float, default=1.0)
+parser.add_argument("--gamma", dest="gamma", type=int, default=1)
+parser.add_argument("--delta", dest="delta", type=float, default=1.0)
+parser.add_argument("--cutoff", dest="cutoff", type=int, default=12)
+
+# An identifier for this run of this script.
+parser.add_argument("--idForProcess", dest="idForProcess", type=int, default=random.randint(0,10000000))
+
+
+
+args=parser.parse_args()
+print(args)
+
+
+assert args.alpha >= 0
+assert args.alpha <= 1
+assert args.delta >= 0
+assert args.gamma >= 1
+
+myID = args.idForProcess
+
+
+TARGET_DIR = "estimates/"
+
+import turkish_segmenter_coarse
+# Translate a verb into an underlying morpheme
+def getRepresentation(lemma):
+   # return turkish_segmenter.get_abstract_morphemes(lemma)
+   return lemma["coarse"]
+
+def getSurprisalRepresentation(lemma):
+   return lemma["fine"]
+
+import turkish_noun_segmenter_coarse
+import turkish_noun_segmenter
+def processVerb(verb, data_):
+    # assumption that each verb is a single word
+   for vb in verb:
+      labels = vb["morph"]
+      morphs = turkish_noun_segmenter_coarse.get_abstract_morphemes(labels)
+      fine = turkish_noun_segmenter.get_abstract_morphemes(labels)
+      morphs[0] = vb["lemma"] # replace "ROOT" w actual root
+      fine[0] = vb["lemma"] # replace "ROOT" w actual root
+      morph_dict = {"fine": fine, "coarse": morphs}
+      data_.append(morph_dict)
+
+# Load both training (for fitting n-gram model) and held-out dev (for evaluating cross-entropy) data
+corpusTrain = CorpusIterator_V(args.language,"train", storeMorph=True).iterator(rejectShortSentences = False)
+corpusDev = CorpusIterator_V(args.language,"dev", storeMorph=True).iterator(rejectShortSentences = False)
+
+pairs = set()
+counter = 0
+data_train = []
+data_dev = []
+for corpus, data_ in [(corpusTrain, data_train), (corpusDev, data_dev)]:
+  for sentence in corpus:
+    verb = []
+    for line in sentence:
+       if line["posUni"] == "NOUN":
+          verb.append(line)
+          processVerb(verb, data_)
+          verb = []
+
+words = []
+
+# Collect morphemes into itos and stoi. These morphemes will be used to parameterize ordering (for Korean, we could use underlying morphemes or the coarse-grained labels provided in Kaist like ef, etm, etc.)
+affixFrequency = {}
+for verbWithAff in data_train:
+  verbWithAff = getRepresentation(verbWithAff)
+  for affix in verbWithAff[1:]:
+    affixFrequency[affix] = affixFrequency.get(affix, 0)+1
+
+
+itos = set()
+for verbWithAff in data_train:
+ verbWithAff = getRepresentation(verbWithAff)
+ for affix in verbWithAff[1:]:
+    itos.add(affix)
+itos = sorted(list(itos))
+stoi = dict(list(zip(itos, range(len(itos)))))
+
+itos_ = itos[::]
+shuffle(itos_)
+if args.model == "RANDOM": # Construct a random ordering of the morphemes
+  weights = dict(list(zip(itos_, [2*x for x in range(len(itos_))])))
+elif args.model in ["REAL", "REVERSE"]: # Measure tradeoff for real or reverse ordering of suffixes.
+  weights = None
+elif args.model != "REAL": # Load the ordering from a file
+  weights = {}
+  import glob
+  files = glob.glob(args.model)
+  assert len(files) == 1
+  assert "Normalized" in files[0]
+  with open(files[0], "r") as inFile:
+     next(inFile)
+     for line in inFile:
+        morpheme, weight = line.strip().split(" ")
+        weights[morpheme] = int(weight)
+
+
+def calculateTradeoffForWeights(weights):
+    train = []
+    dev = []
+    # Iterate through the verb forms in the two data partitions, and linearize as a sequence of underlying morphemes
+    for data, processed in [(data_train, train), (data_dev, dev)]:
+      for verb in data:
+         affixes = verb["coarse"][1:]
+         if args.model == "REAL": # Real ordering
+            _ = 0
+         elif args.model == "REVERSE": # Reverse affixes
+            affixes = affixes[::-1]
+         else: # Order based on weights
+            affixes = sorted(affixes, key=lambda x:weights.get(x, 0))
+
+
+         for ch in verb["fine"]: # Express as a sequence of underlying morphemes (could also instead be a sequence of phonemes if we can phonemize the Korean input)
+            processed.append(ch)
+         processed.append("EOS") # Indicate end-of-sequence
+         for _ in range(args.cutoff+2): # Interpose a padding symbol between each pair of successive verb forms. There is no relation between successive verb forms, and adding padding prevents the n-gram models from "trying to learn" any spurious relations between successive verb forms.
+           processed.append("PAD")
+         processed.append("SOS") # start-of-sequence for the next verb form
+    
+    # Calculate AUC and the surprisals over distances (see estimateTradeoffHeldout.py for further documentation)
+    auc, devSurprisalTable = calculateMemorySurprisalTradeoff(train, dev, args)
+
+
+    # Write results to a file
+    model = args.model
+    outpath = TARGET_DIR+args.language+"_"+__file__+"_model_"+str(myID)+"_"+model+".txt"
+    print(outpath)
+    with open(outpath, "w") as outFile:
+       print(str(args), file=outFile)
+       print(" ".join(map(str,devSurprisalTable)), file=outFile)
+    return auc
+   
+auc = calculateTradeoffForWeights(weights)
+print("AUC: ", auc)
